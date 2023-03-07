3ee2ccb445d6c7a791ddceeb3e8a6b6de0b72296
David Smith <david@david-smith.org>
2023-03-07 10:04:46 +0000

    Initial Commit

---
+++ IsoLines/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IsoLines
+//
+//  Created by David Smith on 3/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IsoLines/IsoLinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsoLinesApp.swift
+//  IsoLines
+//
+//  Created by David Smith on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct IsoLinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
